69727d9d29e3950f03e198a21e914038a6d86331
Chalapathi <chalapathiks@gmail.com>
2024-01-29 15:38:20 +0530

    committing Test3 to BranchX

---
+++ src/Test3.java
@@ -0,0 +1,9 @@
+
+public class Test3 {
+	public static void main(String[] args) {
+		String s="Deeksha";
+		if(true)
+			System.out.println("Friends");
+		else
+			System.out.println("Strangers");
+}
