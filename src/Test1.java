87b988bc3bb95e70ffb5a4e901c0db3b99ea0f19
Chalapathi <chalapathiks@gmail.com>
2024-01-29 14:10:29 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String s="Deeksha";
+		if(true)
+			System.out.println("Friends");
+		else
+			System.out.println("Strangers");
+	}
+}
---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+		String s="Deeksha";
+		if(true)
+			System.out.println("Friends");
+		else
+			System.out.println("Strangers");
+	}
+}
